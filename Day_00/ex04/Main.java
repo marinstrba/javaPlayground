5bbd80685b35fda95ecb7c8b5b3d3b4bca0ee9ca
Marian Strba <marinstrba@gmail.com>
2023-11-02 11:47:33 +0100

    Update: I added a checkInput function. And currently working on the main algorithm of the code which will determine the number of chars in the string.

--- Day_00/ex04/Main.java
+++ Day_00/ex04/Main.java
@@ -1,9 +1,43 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
+		int			sum;
+		char		letter;
+		String 		input;
+		List<Integer> frequency = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
+
+		input = checkInput(scanner);
+		while (true)
+		{
+			sum = getLetterUsage(input);
+			frequency.add()
+		}
+		scanner.close();
+	}
+
+	private static String checkInput (Scanner scanner)
+	{
+		String input;
+		System.out.println("Please input your text:");	
+		input = scanner.nextLine();
+		return (input);
+	}
+
+	private static int getLetterUsage (String string)
+	{
+		int sum;
+
+		sum = 0;
+		for (int i = 0; i < string.length(); ++i)
+		{
+			
+		}
 
 	}
 }
