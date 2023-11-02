919c9c538f081c9c505235d654e08e369853898a
Marian Strba <marinstrba@gmail.com>
2023-11-02 16:35:46 +0100

    Final version of my code.

---
+++ Day_01/ex01/Main.java
@@ -0,0 +1,13 @@
+public class Program 
+{
+    public static void main(String[] args)
+	{
+        User p1 = new User("Mike",  2600);
+        User p2 = new User("John",  1000);
+        User p3 = new User("Nikol", 1500);
+
+        p1.printConsole();
+        p2.printConsole();
+        p3.printConsole();
+    }
+}
